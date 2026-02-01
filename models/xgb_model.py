f7ec73e97c3f05490e8b61419c3d88ec7a91f57e
Kush05Bhardwaj <kush2012bhardwaj@gmail.com>
2026-02-01 23:59:50 +0530

    -- udate the ml (xgb) model

--- models/xgb_model.py
+++ models/xgb_model.py
@@ -4,6 +4,9 @@ import numpy as np
 from sklearn.metrics import mean_squared_error, mean_absolute_error 
 from xgboost import XGBRegressor
 from prepare_data import create_features, create_features_with_sentiment
+from risk.volatility import calculate_volatility
+from risk.confidence import full_risk_assessment
+
 
 df = pd.read_csv('data/processed/price_with_sentiment.csv')
 prices = df['Close'].values
@@ -28,16 +31,16 @@ mse = mean_squared_error(y_test, y_pred)
 mae = mean_absolute_error(y_test, y_pred)
 rmse = np.sqrt(mse)
 
+vol = calculate_volatility(df['Close'].values)
+report = full_risk_assessment(
+    predicted_price=preds[-1],
+    rmse=rmse,
+    volatility=vol,
+    sentiment_data=df['Sentiment'].values
+)
+
 print(f"XGBoost Model Performance:")
 print(f"Mean Squared Error: {mse}")
 print(f"Mean Absolute Error: {mae}")
 print(f"Root Mean Squared Error: {rmse}")
-
-
-if sentiment_data < -0.3:
-    confidence = "Low (High Negative Sentiment)"
-elif sentiment_data > 0.3:
-    confidence = "High (High Positive Sentiment)"
-else:
-    confidence = "Medium (Neutral Sentiment)"
     
