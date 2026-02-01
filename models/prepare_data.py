54f283a1a47d286653c390bc1234884757dd0f5f
Kush05Bhardwaj <kush2012bhardwaj@gmail.com>
2026-02-01 23:25:58 +0530

    sentiment feature in models

--- models/prepare_data.py
+++ models/prepare_data.py
@@ -7,3 +7,13 @@ def create_features(df, window=50):
         X.append(df['Close'].values[i-window:i])
         y.append(df['Close'].values[i])
     return np.array(X), np.array(y)
+
+def create_feature_with_sentiment(df, window=5):
+    X, y = [], []
+    for i in range(window, len(df)):
+        prices = df['Close'].values[i-window:i]
+        sentiment = df["Sentiment"].values[i-window:i]
+        features = np.concatenate([prices, sentiment])
+        X.append(features)
+        y.append(df['Close'].values[i])
+    return np.array(X), np.array(y)
